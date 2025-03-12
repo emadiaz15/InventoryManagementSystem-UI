99dbb8c6402cf789bf5287d5eb9849f5d3c0f738
emadiaz15 <e.diaz@itecriocuarto.org.ar>
2025-03-12 14:42:20 -0300

    `Update Vite configuration to use environment variables for port and API URL`

--- vite.config.js
+++ vite.config.js
@@ -2,19 +2,21 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   server: {
-    port: 5174, // Asegura que el frontend corre en el puerto 5174
+    // El puerto es configurado por Railway en la variable de entorno PORT
+    port:  import.meta.env.PORT || 5174, // Si el entorno no define el puerto, usa 5174 por defecto
     strictPort: true, // Evita cambiar de puerto automáticamente si está ocupado
     proxy: {
       '/api': {
-        target: 'http://localhost:8000', // Cambia este puerto si tu backend está en otro
+        target: import.meta.env.VITE_API_URL || 'http://localhost:8000', // Usa la variable de entorno VITE_API_URL o localhost en desarrollo
         changeOrigin: true,
         secure: false, // Permite peticiones a HTTP sin SSL en desarrollo
-        rewrite: (path) => path.replace(/^\/api/, ''),
+        rewrite: (path) => path.replace(/^\/api/, ''), // Reescribe la ruta del proxy
       },
     },
   },
   preview: {
-    port: 4174, // Puerto para el modo preview
+    // Puerto para el modo preview
+    port: 4174,
     strictPort: true, // Evita cambiar de puerto automáticamente si está ocupado
   },
 });
