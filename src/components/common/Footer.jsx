cc6fb01b92eee2c1c356122c0821a1d4aa2f8b4e
emadiaz15 <e.diaz@itecriocuarto.org.ar>
2025-03-18 22:35:14 -0300

    "changes added to the footer"

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -4,7 +4,8 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className="footer flex justify-center bg-primary text-black p-4">
-      <p>&copy; 2024 Mi Aplicación. Todos los derechos reservados.</p>
+      <p>&copy; 2025 Mi Aplicación. Todos los derechos reservados. Desarrollado por Emanuel Diaz
+      </p>
     </footer>
   );
 };
