77a26f7aaba11797991d0a0dda00346eec85c5fe
emadiaz15 <e.diaz@itecriocuarto.org.ar>
2025-03-19 00:57:08 -0300

    Remove unused React import from components

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,9 +2,9 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
+    'eslint-plugin-react',
     'plugin:react-hooks/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
