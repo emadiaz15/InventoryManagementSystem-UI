b1aa0e1ce52834a2987aa8e2be78717ad4f7cb99
emadiaz15 <e.diaz@itecriocuarto.org.ar>
2025-03-24 18:09:49 -0300

    fix error deploy

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,33 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: parseInt(process.env.VITE_PORT) || 5173, // Usa el puerto de Railway o 5173 por defecto
-    strictPort: true, // Evita cambiar de puerto si está ocupado
-    host: "0.0.0.0", // Permite acceso desde cualquier IP dentro de Docker
-    watch: {
-      usePolling: true, // Permite detectar cambios dentro del contenedor Docker
-    },
-    hmr: {
-      host: "localhost", // Evita problemas con Hot Module Replacement
-      protocol: "ws", // Asegura que Vite use WebSockets para el reload
-    },
-    proxy: {
-      "/api": {
-        target: process.env.VITE_API_BASE_URL || "http://localhost:8000/api/v1",
-        changeOrigin: true,
-        secure: false, // Permite peticiones sin HTTPS en desarrollo
-        rewrite: (path) => path.replace(/^\/api/, ""),
+export default defineConfig(({ command, mode }) => {
+  return {
+    base: mode === "production" ? "https://inventarioweb.up.railway.app/" : "/",
+    plugins: [react()],
+    server: {
+      port: parseInt(process.env.VITE_PORT) || 5173,
+      strictPort: true,
+      host: "0.0.0.0",
+      watch: {
+        usePolling: true,
+      },
+      hmr: {
+        host: "localhost",
+        protocol: "ws",
+      },
+      proxy: {
+        "/api": {
+          target: process.env.VITE_API_BASE_URL || "http://localhost:8000/api/v1",
+          changeOrigin: true,
+          secure: false,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
       },
     },
-  },
-  preview: {
-    port: 4174,
-    strictPort: true,
-  },
+    preview: {
+      port: 4174,
+      strictPort: true,
+    },
+  };
 });
