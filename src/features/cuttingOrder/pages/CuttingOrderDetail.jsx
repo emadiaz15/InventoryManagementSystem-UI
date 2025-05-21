106cc8211f2642bee6a25d9a7df298494bb2097a
emadiaz15 <e.diaz@itecriocuarto.org.ar>
2025-05-20 22:23:20 -0300

    improve initial loading UX with full-screen spinner and persistent filters

--- src/features/cuttingOrder/pages/CuttingOrderDetail.jsx
+++ src/features/cuttingOrder/pages/CuttingOrderDetail.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
-import Layout from '../../pages/Layout';
+import Layout from '../../../pages/Layout';
 import Spinner from '../../components/ui/Spinner';
 import ErrorMessage from '../../components/common/ErrorMessage';
 
