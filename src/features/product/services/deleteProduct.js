5725b78499282f2f081f1abcbd53673628062ec8
emadiaz15 <e.diaz@itecriocuarto.org.ar>
2025-07-01 23:05:57 -0300

    fix error features update and delete products

--- src/features/product/services/deleteProduct.js
+++ src/features/product/services/deleteProduct.js
@@ -5,9 +5,10 @@ import { djangoApi } from "@/api/clients";
  * Usa baseURL='/api/v1' + esta ruta => '/api/v1/inventory/products/:id/'
  */
 export const deleteProduct = async (productId) => {
+  const id = String(productId).trim();
   try {
     // Coincide con tu updateProduct:
-    const response = await djangoApi.delete(`/inventory/products/${productId}/`);
+    const response = await djangoApi.delete(`/inventory/products/${id}/`);
     // axios lanza si no es 2xx, así que si llega aquí, fue 204 o 200
     return response;
   } catch (error) {
--- src/features/product/services/updateProduct.js
+++ src/features/product/services/updateProduct.js
@@ -11,12 +11,13 @@ export const updateProduct = async (productId, productData) => {
   if (!productId || typeof productData !== "object") {
     throw new Error("❌ Se requiere un productId y un objeto de datos válido.");
   }
+  const id = String(productId).trim();
 
   try {
     const isForm = productData instanceof FormData;
 
     const response = await djangoApi.put(
-      `/inventory/products/${productId}/`,
+      `/inventory/products/${id}/`,
       productData,
       isForm ? { headers: { "Content-Type": "multipart/form-data" } } : {}
     );
